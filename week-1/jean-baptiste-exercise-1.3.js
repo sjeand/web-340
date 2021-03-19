3980ceef0bd73f520222b91ed630ccecf6001678
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-03-19 17:10:59 -0500

    week-1 update

--- week-1/jean-baptiste-exercise-1.3.js
+++ week-1/jean-baptiste-exercise-1.3.js
@@ -2,13 +2,15 @@
 =======================================
 ;Title: Assignment 1.3
 ;Author: Sarah Jean Baptiste
-;Date: 03/15/2021
-;Description:
+;Date: 03/18/2021
+;Description: Week-1 Module
 =======================================
 */
 
+// Require URL function.
 var url = require('url');
 
+// Parse URL address.
 var parsedURL = url.parse('https://github.com/sjeand?name=jeanBaptiste');
 
 console.log(parsedURL.protocol);
