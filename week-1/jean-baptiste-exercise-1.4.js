98a54b45552f21d6c12a9de1fe59d6ae2f9ea912
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-03-21 21:58:49 -0500

    1.5

--- week-1/jean-baptiste-exercise-1.4.js
+++
@@ -1,9 +0,0 @@
-/*
-=======================================
-;Title: Assignment 1.4
-;Author: Sarah Jean Baptiste
-;Date: 03/20/2021
-;Description: Week-1 REPL in action
-=======================================
-*/
-
