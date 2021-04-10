684334547eecc8e7b093fcf07d1e6c3cac255193
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-04-10 09:07:36 -0500

    JSON APIs

---
+++ week-4/json-api/jeanBaptiste-exercise-4.2.js
@@ -0,0 +1,37 @@
+/*
+=======================================================
+  Title: jeanBaptiste-exercise-4.2.js
+  Author: Professor Cross
+  Date: 04/10/2021
+  Modified by: Sarah Jean Baptiste
+  Description: JSON APIs
+========================================================
+*/
+
+// Import libraries
+var express = require ('express')
+var http = require('http');
+
+
+var app = express();
+
+// Add logger
+//app.use(logger('dev'));
+
+// Get request with product info and return
+app.get('/product/:id', function(req, res){
+    
+    var id = parseInt(req.params.id, 10);
+
+    res.json({
+
+        productName: 'Fiddle Leaf Fig',
+        productPrice: '$49.99',
+        productId: id, 
+    });
+});
+
+// Create server on port 3000
+http.createServer(app).listen(3000, function(){
+    console.log('Application started and listening on port 3000') // Message to recognize server is started and listening.
+});
